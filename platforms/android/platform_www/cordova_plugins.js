967a9ad6cf0c3195878789706ecd91881c251ce7
Trennon Dunn <trennon.dunn@mail.ventrux.com>
2015-11-21 17:29:17 -0600

    gen

--- platforms/android/platform_www/cordova_plugins.js
+++ platforms/android/platform_www/cordova_plugins.js
@@ -7,19 +7,19 @@ module.exports = [
         "runs": true
     },
     {
-        "file": "plugins/cordova-plugin-splashscreen/www/splashscreen.js",
-        "id": "cordova-plugin-splashscreen.SplashScreen",
-        "pluginId": "cordova-plugin-splashscreen",
-        "clobbers": [
-            "navigator.splashscreen"
-        ]
-    },
-    {
         "file": "plugins/cordova-plugin-pushnotification-parsepushnotification/www/parsepushnotification.js",
         "id": "cordova-plugin-pushnotification-parsepushnotification.parsepushnotification",
         "pluginId": "cordova-plugin-pushnotification-parsepushnotification",
         "clobbers": [
             "window.parsepushnotification"
+        ]
+    },
+    {
+        "file": "plugins/cordova-plugin-splashscreen/www/splashscreen.js",
+        "id": "cordova-plugin-splashscreen.SplashScreen",
+        "pluginId": "cordova-plugin-splashscreen",
+        "clobbers": [
+            "navigator.splashscreen"
         ]
     }
 ];
--- platforms/wp8/platform_www/cordova_plugins.js
+++ platforms/wp8/platform_www/cordova_plugins.js
@@ -1,19 +1,19 @@
 cordova.define('cordova/plugin_list', function(require, exports, module) {
 module.exports = [
     {
-        "file": "plugins/cordova-plugin-splashscreen/www/splashscreen.js",
-        "id": "cordova-plugin-splashscreen.SplashScreen",
-        "pluginId": "cordova-plugin-splashscreen",
-        "clobbers": [
-            "navigator.splashscreen"
-        ]
-    },
-    {
         "file": "plugins/cordova-plugin-pushnotification-parsepushnotification/www/parsepushnotification.js",
         "id": "cordova-plugin-pushnotification-parsepushnotification.parsepushnotification",
         "pluginId": "cordova-plugin-pushnotification-parsepushnotification",
         "clobbers": [
             "window.parsepushnotification"
+        ]
+    },
+    {
+        "file": "plugins/cordova-plugin-splashscreen/www/splashscreen.js",
+        "id": "cordova-plugin-splashscreen.SplashScreen",
+        "pluginId": "cordova-plugin-splashscreen",
+        "clobbers": [
+            "navigator.splashscreen"
         ]
     }
 ];
